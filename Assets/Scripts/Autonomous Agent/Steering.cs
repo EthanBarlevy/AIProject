b777fd55cd6a946df325e21c7c47ecc530ca72db
EthanBarlevy <98473724+EthanBarlevy@users.noreply.github.com>
2023-01-11 19:16:45 -0700

    Spawner layer for agents

--- Assets/Scripts/Autonomous Agent/Steering.cs
+++ Assets/Scripts/Autonomous Agent/Steering.cs
@@ -14,6 +14,20 @@ public static class Steering
         return CalculateSteering(agent, (agent.transform.position - target.transform.position));
     }
 
+    public static Vector3 Wander(AutonomousAgent agent)
+    {
+        // random angle adjust
+        agent.wanderAngle = agent.wanderAngle + Random.Range(-agent.wanderDisplacement, agent.wanderDisplacement);
+        // create rotation (around y-axis)
+        Quaternion rotation = Quaternion.AngleAxis(agent.wanderAngle, Vector3.up);
+        // calculate point on circle
+        Vector3 point = rotation * (Vector3.forward * agent.wanderRadius);
+        // set point in front of agent
+        Vector3 forward = agent.transform.forward * agent.wanderDistance;
+
+        return CalculateSteering(agent, forward + point);
+    }
+
     public static Vector3 CalculateSteering(Agent agent, Vector3 direction)
     {
         Vector3 desired = direction.normalized * agent.movement.maxSpeed;
