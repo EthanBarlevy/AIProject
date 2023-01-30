3706d8457ea9a71c63e6bf81bf4481c562a8ce44
EthanBarlevy <98473724+EthanBarlevy@users.noreply.github.com>
2023-01-29 20:25:21 -0700

    Nav mesh kinda but also not really becaues half of the scripts didnt work at all

---
+++ Assets/Scripts/Nav Agent/NavAgentPlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavAgentPlayerController : MonoBehaviour
+{
+    public NavMeshSurface surface;
+
+    public Camera cam;
+    public NavMeshAgent agent;
+
+    void Start()
+    {
+        surface.BuildNavMesh();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                // move agent
+                agent.SetDestination(hit.point);
+            }
+
+        }
+    }
+}
